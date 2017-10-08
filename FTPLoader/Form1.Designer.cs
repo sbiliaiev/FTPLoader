a35cb12cbbc9d897274197c3d1038225c94ae6f7
Serhii Biliaiev <serhii.biliaiev@gmail.com>
2017-10-08 20:31:42 +0300

    initial commit

---
+++ FTPLoader/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace FTPLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "one",
+            "two",
+            "three",
+            "four",
+            "five"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(1027, 571);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(419, 595);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 652);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FTPLoader/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FluentFTP;
+using System.Net;
+
+namespace FTPLoader
+{
+    public partial class Form1 : Form
+    {
+        FtpClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string test = "test string";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FtpClient("127.0.0.1");
+            client.Port = 54218;
+            client.Credentials = new NetworkCredential("dev_user", "dev_user_pass");
+            client.Connect();
+
+            List<string> cityList = new List<string>();
+
+            // get a list of files and directories in the "/htdocs" folder
+            foreach (FtpListItem item in client.GetListing("/"))
+            {
+
+                // if this is a file
+                if (item.Type == FtpFileSystemObjectType.File)
+                {
+
+                    // get the file size
+                    long size = client.GetFileSize(item.FullName);
+                    Console.WriteLine(size);
+
+                }
+
+                // get modified date/time of the file or folder
+                DateTime time = client.GetModifiedTime(item.FullName);
+
+                // calculate a hash for the file on the server side (default algorithm)
+                // FtpHash hash = client.GetHash(item.FullName);
+                Console.WriteLine(item.FullName);
+                cityList.Add(item.Name);
+            }
+
+
+            checkedListBox1.DataSource = cityList;
+
+            string str = "573Report20170930050115_AE";
+            Console.WriteLine(str);
+            int index;
+            index = str.IndexOf("Report");
+            Console.WriteLine("index:" + index);
+            string dateStr = str.Substring(index + 6, 8);
+            Console.WriteLine("date = " + dateStr);
+            DateTime currentDate = DateTime.Now;
+            string currentDateStr = currentDate.ToString("yyyyMMdd");
+            Console.WriteLine("current date str = " + currentDateStr);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*List<string> checkedItems = new List<string>();
+            foreach (var item in checkedListBox1.CheckedItems)
+                checkedItems.Add(item.ToString());*/
+
+            //if (e.NewValue == CheckState.Checked)
+              //  checkedItems.Add(checkedListBox1.Items[e.Index].ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var city in checkedListBox1.CheckedItems)
+            {
+                foreach (FtpListItem item in client.GetListing(city.ToString() + "/Reports"))
+                {
+                    Console.WriteLine("item in city" + item.Name);
+                }
+            }
+        }
+    }
+}
